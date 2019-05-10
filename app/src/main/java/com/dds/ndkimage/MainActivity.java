b1910de9cc21259166dae8e2b49b8411a406f0ed
dds <android_shuai@163.com>
2019-05-10 13:01:24 +0800

    權限

--- app/src/main/java/com/dds/ndkimage/MainActivity.java
+++ app/src/main/java/com/dds/ndkimage/MainActivity.java
@@ -28,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (!PermissionUtil.isNeedRequestPermission(this)) {
+
+        }
     }
 
 
